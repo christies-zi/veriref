b31f919e69011f2f9b1abd13d3323a72b800f99e
kz1021 <kristina.zimina21@imperial.ac.uk>
2024-12-17 19:39:35 +0300

    Display verification by separate claims

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import axios from "axios";
 import "./styles/App.css";
 
 function App() {
+  const isLocal = true;
+  const BACKEND_SERVER = isLocal ? "http://127.0.0.1:5000" : process.env.REACT_APP_BACKEND_SERVER;
   const [sourceFileInput, setSourceFileInput] = useState(null); // Stores the uploaded PDF file
   const [sourceTextInput, setSourceTextInput] = useState(""); // Stores plain text input
   const [toVerify, setToVerify] = useState(""); // Second input
@@ -10,6 +12,10 @@ function App() {
   const [explanation, setExplanation] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [claims, setClaims] = useState([]);
+  const [claimsAnswers, setClaimsAnswers] = useState([]);
+  const [claimsExplanations, setClaimsExplanations] = useState([]);
+  const [claimsSentences, setClaimsSentences] = useState([]);
 
   const handleFileUpload = (e) => {
     const file = e.target.files[0];
@@ -38,6 +44,10 @@ function App() {
     setShortAnswer("");
     setExplanation("");
     setIsLoading(true);
+    setClaims([]);
+    setClaimsAnswers([]);
+    setClaimsExplanations([]);
+    setClaimsSentences([]);
 
     try {
       const formData = new FormData();
@@ -48,14 +58,31 @@ function App() {
       }
       formData.append("toVerify", toVerify); // Include Information to be verified
 
-      const response = await axios.post("https://veriref-server/process", formData, {
+      const response = await axios.post(`${BACKEND_SERVER}/process`, formData, {
         headers: {
-          "Access-Control-Allow-Origin": "https://veriref-server/",
+          "Access-Control-Allow-Origin": BACKEND_SERVER,
           "Content-Type": "multipart/form-data",
         },
       });
+
       setShortAnswer(response.data.shortAnswer);
       setExplanation(response.data.explanation);
+      console.log(response.data.claims);
+      let claimsLoc = [];
+      let answers = [];
+      let explanations = [];
+      let sentences = [];
+      response.data.claims.forEach(function(element) {
+        claimsLoc.push(element[0]);
+        answers.push(element[1]);
+        explanations.push(element[2]);
+        sentences.push(element[3]);
+      });
+
+      setClaims(claimsLoc);
+      setClaimsAnswers(answers);
+      setClaimsExplanations(explanations);
+      setClaimsSentences(sentences);
     } catch (error) {
       console.error("Error processing inputs:", error);
     } finally {
@@ -66,6 +93,19 @@ function App() {
   return (
     <div className="app-container">
       <h1>Veriref</h1>
+
+      <div className="input-group">
+          <label htmlFor="toVerify" className="input-label">
+            Information to Verify (Text):
+          </label>
+          <textarea
+            placeholder="Enter information to be verified"
+            value={toVerify}
+            onChange={handleToVerifyInput}
+            className="input-textarea"
+            rows="4"
+          />
+        </div>
 
       <div className="input-section">
         <div className="input-group">
@@ -87,19 +127,6 @@ function App() {
             rows="4"
           />
         </div>
-
-        <div className="input-group">
-          <label htmlFor="toVerify" className="input-label">
-            Information to Verify (Text):
-          </label>
-          <textarea
-            placeholder="Enter information to be verified"
-            value={toVerify}
-            onChange={handleToVerifyInput}
-            className="input-textarea"
-            rows="4"
-          />
-        </div>
       </div>
 
       {errorMessage && <p className="error-message">{errorMessage}</p>}
@@ -112,10 +139,21 @@ function App() {
 
       {shortAnswer && (
         <div className="output-section">
-          <h3>The input information is: {shortAnswer}</h3>
-          <p>{explanation}</p>
+          <h3>The input information can be split into the following claims:</h3>
         </div>
       )}
+
+      {claims.map((claim, i) => (
+          <div className="output-section">
+            <h3>{claim}</h3>
+            <div>{claimsAnswers[i]}</div>
+            <p>Explanation: {claimsExplanations[i]}</p>
+            <p>Reference sentences:</p>
+            <p>{claimsSentences[i].toString()}</p>
+          </div>
+          ))}
+
+
     </div>
   );
 }
